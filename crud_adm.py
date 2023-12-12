b0fdeb073e0f5856034dd217fc9429029ea0761e
Mutia Yasinta <mutiayasinta18@gmail.com>
2023-12-13 06:39:25 +0700

    baru

--- crud_adm.py
+++ crud_adm.py
@@ -9,11 +9,11 @@ laundry = [
 
 # Dummy data paket
 paket_cuci = [
-    {"id_laundry": 1, "nama_paket": "paket 1", "durasi": 3, "harga": 5000, "unit": "2 kg"},
-    {"id_laundry": 2, "nama_paket": "paket hemat", "durasi": 2, "harga": 6000, "unit": "5 kg"},
-    {"id_laundry": 3, "nama_paket": "pahe a", "durasi": 5, "harga": 7000, "unit": "1 kg"},
-    {"id_laundry": 4, "nama_paket": "murmer", "durasi": 4, "harga": 8000, "unit": "3 kg"},
-    {"id_laundry": 5, "nama_paket": "murah", "durasi": 2, "harga": 9000, "unit": "4 kg"}
+    {"id_laundry": 1, "nama_paket": "paket 1", "durasi": 3, "harga": 5000, "unit": "kg"},
+    {"id_laundry": 2, "nama_paket": "paket hemat", "durasi": 2, "harga": 6000, "unit": "kg"},
+    {"id_laundry": 3, "nama_paket": "pahe a", "durasi": 5, "harga": 7000, "unit": "kg"},
+    {"id_laundry": 4, "nama_paket": "murmer", "durasi": 4, "harga": 8000, "unit": "kg"},
+    {"id_laundry": 5, "nama_paket": "murah", "durasi": 2, "harga": 9000, "unit": "kg"}
 ]
 
 
@@ -43,6 +43,7 @@ def menu_edit_hapus_lihat_paket(id_laundry):
         print("1. Edit Data Laundry")
         print("2. Hapus Data Laundry")
         print("3. Lihat Paket Cuci")
+        print("4. Tambah Paket Cuci")
         print("0. Kembali")
 
         pilihan = input("-> Masukkan pilihan (1/2/3/0): ")
@@ -53,6 +54,8 @@ def menu_edit_hapus_lihat_paket(id_laundry):
             break
         elif pilihan == "3":
             lihat_paket_cuci(id_laundry)
+        elif pilihan == "4":
+            tambah_paket_cuci(id_laundry)
         elif pilihan == "0":
             break
         else:
@@ -76,20 +79,27 @@ def hapus_data_laundry(id_laundry):
     paket_cuci = [p for p in paket_cuci if p['id_laundry'] != id_laundry]
     print("=== Data Laundry berhasil dihapus beserta paket cuci yang terkait. ===")
 
+
 # FUNCTION <LIHAT PAKET CUCI> DARI PILIHAN MENU 
 def lihat_paket_cuci(id_laundry):
     paket_cuci_laundry = [p for p in paket_cuci if p['id_laundry'] == id_laundry]
     
     print("\n<< Paket Cuci: >>")
-    for paket in paket_cuci_laundry:
-        print(f"Nama Paket: {paket['nama_paket']}")
+    for idx, paket in enumerate(paket_cuci_laundry, start=1):
+        print(f"{idx}. Nama Paket: {paket['nama_paket']}")
 
     while True:
-        pilihan_paket = input("--> Masukkan nama paket Laundry (0 untuk kembali): ")
+        pilihan_paket = input("--> Masukkan angka paket Laundry (0 untuk kembali): ")
         if pilihan_paket == "0":
             break
         else:
-            lihat_detail_paket_cuci(id_laundry, pilihan_paket)
+            pilihan_paket = int(pilihan_paket)
+            if 1 <= pilihan_paket <= len(paket_cuci_laundry):
+                selected_paket = paket_cuci_laundry[pilihan_paket - 1]
+                lihat_detail_paket_cuci(id_laundry, selected_paket['nama_paket'])
+                break
+            else:
+                print("Pilihan tidak valid. Silakan masukkan angka yang benar.")
 
 # DETAIL PAKET CUCI YANG SEBELUMNYA DIINPUTKAN USER
 def lihat_detail_paket_cuci(id_laundry, nama_paket):
@@ -101,6 +111,25 @@ def lihat_detail_paket_cuci(id_laundry, nama_paket):
         menu_edit_hapus_paket_cuci(id_laundry, nama_paket)
     else:
         print("Paket Cuci tidak ditemukan.")
+
+
+# TAMBAH PAKET CUCI
+def tambah_paket_cuci(id):
+    print("\n>>> Masukkan Paket Laundry <<<")
+    id_laundry = len(paket_cuci) + 1
+    nama_paket = input("Masukkan nama paket cuci => ")
+    durasi = int(input("Masukkan durasi pengerjaan paket cuci (dalam hari) => "))
+    unit = str(input("Masukkan unit paket cuci (satuan/pasang/kg) => "))
+    harga = int(input("Masukkan harga paket cuci per unit => "))
+    
+    paket_cuci.append({
+        "id_laundry": id_laundry,
+        "nama_paket": nama_paket,
+        "durasi": durasi,
+        "unit": unit,
+        "harga": harga,
+    })
+    print(f"=== Paket Cuci {nama_paket} berhasil ditambahkan ===\n")
 
 
 ### MENU EDIT DAN HAPUS PAKET CUCI <PAKET CUCI> MUNCUL APABILA DI AWAL SUDAH MEMILIH LAUNDRY YANG DITAMPILKAN ###
@@ -126,13 +155,15 @@ def menu_edit_hapus_paket_cuci(id_laundry, nama_paket):
 def edit_paket_cuci(id_laundry, nama_paket):
     selected_paket = next((p for p in paket_cuci if p['id_laundry'] == id_laundry and p['nama_paket'] == nama_paket), None)
     if selected_paket:
-        print("\nEdit Paket Cuci:")
+        print("\n>> Edit Paket Cuci: <<")
+        selected_paket['nama_paket'] = input(f"Masukkan nama paket baru ({selected_paket['nama_paket']}): ") or selected_paket['nama_paket']
         selected_paket['durasi'] = int(input(f"Masukkan durasi baru ({selected_paket['durasi']}): ") or selected_paket['durasi'])
         selected_paket['harga'] = int(input(f"Masukkan harga baru ({selected_paket['harga']}): ") or selected_paket['harga'])
-        selected_paket['unit'] = int(input(f"Masukkan unit baru ({selected_paket['unit']}): ") or selected_paket['unit'])
+        selected_paket['unit'] = str(input(f"Masukkan unit baru ({selected_paket['unit']}): ") or selected_paket['unit'])
         print("=== Data Paket Cuci berhasil diubah. ===")
     else:
         print("Paket Cuci tidak ditemukan.")
+
 
 # FUNCTION HAPUS PAKET CUCI
 def hapus_paket_cuci(id_laundry, nama_paket):
@@ -151,3 +182,4 @@ while True:
         break
 
     tampilkan_detail_laundry(id_laundry_pilihan)
+
